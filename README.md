7d6888fe637a93df40bcdc94074dfdd48d646569
Elb1to <elbitomc@gmail.com>
2021-05-06 11:58:56 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SoupPvP
+ Lunar Network SoupPvP gamemode replica
---
+++ src/main/java/me/elb1to/souppvp/SoupPvP.java
@@ -0,0 +1,16 @@
+package me.elb1to.souppvp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SoupPvP extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+}
