f89411200c03fc6e83125ebad0985223eaf87daa
Elb1to <elbitomc@gmail.com>
2021-05-06 13:17:50 -0500

    Webhook Test
    Testing if the Discord Webhook is properly working...

--- src/main/java/me/elb1to/souppvp/SoupPvP.java
+++ src/main/java/me/elb1to/souppvp/SoupPvP.java
@@ -1,12 +1,16 @@
 package me.elb1to.souppvp;
 
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
+@Getter
 public final class SoupPvP extends JavaPlugin {
+
+	@Getter private static SoupPvP instance;
 
 	@Override
 	public void onEnable() {
-
+		instance = this;
 	}
 
 	@Override
