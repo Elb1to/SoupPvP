f3109e4071d29f8f458333421406aab101ca8c7d
Elb1to <elbitomc@gmail.com>
2021-05-13 18:11:59 -0500

    Executing this command will now open the Kit Selection GUI

--- src/main/java/me/elb1to/souppvp/commands/user/KitCommand.java
+++ src/main/java/me/elb1to/souppvp/commands/user/KitCommand.java
@@ -1,10 +1,9 @@
 package me.elb1to.souppvp.commands.user;
 
-import me.elb1to.souppvp.SoupPvP;
+import me.elb1to.souppvp.user.ui.kit.KitSelectionMenu;
 import me.elb1to.souppvp.utils.command.BaseCommand;
 import me.elb1to.souppvp.utils.command.Command;
 import me.elb1to.souppvp.utils.command.CommandArgs;
-import org.bukkit.entity.Player;
 
 /**
  * Created by Elb1to
@@ -13,13 +12,8 @@ import org.bukkit.entity.Player;
  */
 public class KitCommand extends BaseCommand {
 
-    private final SoupPvP plugin = SoupPvP.getInstance();
-
     @Override @Command(name = "kit", aliases = {"kits"})
     public void onCommand(CommandArgs command) {
-        Player player = command.getPlayer();
-        String[] args = command.getArgs();
-
-        // Open Kit Selection menu to player.
+        new KitSelectionMenu().openMenu(command.getPlayer());
     }
 }
