5f49878d25125134b7006fc7f971f7eb25d9c7b3
Elb1to <elbitomc@gmail.com>
2021-05-13 18:00:35 -0500

    Added Menu API :+1:
    This Menu API isn't coded by me, so don't start talkin shit for using it

---
+++ src/main/java/me/elb1to/souppvp/listeners/ButtonListener.java
@@ -0,0 +1,101 @@
+package me.elb1to.souppvp.listeners;
+
+import me.elb1to.souppvp.SoupPvP;
+import me.elb1to.souppvp.utils.menu.Button;
+import me.elb1to.souppvp.utils.menu.Menu;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.InventoryAction;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.metadata.FixedMetadataValue;
+
+public class ButtonListener implements Listener {
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onButtonPress(InventoryClickEvent event) {
+        Player player = (Player) event.getWhoClicked();
+        Menu openMenu = Menu.currentlyOpenedMenus.get(player.getName());
+        if (openMenu != null) {
+            if (event.getSlot() != event.getRawSlot()) {
+                if ((event.getClick() == ClickType.SHIFT_LEFT || event.getClick() == ClickType.SHIFT_RIGHT)) {
+                    event.setCancelled(true);
+                }
+                return;
+            }
+
+            if (openMenu.getButtons().containsKey(event.getSlot())) {
+                Button button = openMenu.getButtons().get(event.getSlot());
+                boolean cancel = button.shouldCancel(player, event.getSlot(), event.getClick());
+                if (!cancel && (event.getClick() == ClickType.SHIFT_LEFT || event.getClick() == ClickType.SHIFT_RIGHT)) {
+                    event.setCancelled(true);
+                    if (event.getCurrentItem() != null) {
+                        player.getInventory().addItem(event.getCurrentItem());
+                    }
+                } else {
+                    event.setCancelled(cancel);
+                }
+
+                button.clicked(player, event.getSlot(), event.getClick(), event.getHotbarButton());
+
+                if (Menu.currentlyOpenedMenus.containsKey(player.getName())) {
+                    Menu newMenu = Menu.currentlyOpenedMenus.get(player.getName());
+                    if (newMenu == openMenu) {
+                        boolean buttonUpdate = button.shouldUpdate(player, event.getSlot(), event.getClick());
+                        if (openMenu.isUpdateAfterClick()) {
+                            openMenu.setClosedByMenu(true);
+                            newMenu.openMenu(player);
+                        }
+                    }
+                } else if (button.shouldUpdate(player, event.getSlot(), event.getClick())) {
+                    openMenu.setClosedByMenu(true);
+                    openMenu.openMenu(player);
+                }
+                if (event.isCancelled()) {
+                    Bukkit.getScheduler().runTaskLater(SoupPvP.getInstance(), player::updateInventory, 1L);
+                }
+            } else {
+                if ((event.getClick() == ClickType.SHIFT_LEFT || event.getClick() == ClickType.SHIFT_RIGHT || event.getAction().equals(InventoryAction.MOVE_TO_OTHER_INVENTORY) || event.getAction().equals(InventoryAction.HOTBAR_MOVE_AND_READD) || event.getAction().equals(InventoryAction.HOTBAR_SWAP))) {
+                    event.setCancelled(true);
+                }
+            }
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onInventoryClose(InventoryCloseEvent event) {
+        Player player = (Player) event.getPlayer();
+        Menu openMenu = Menu.currentlyOpenedMenus.get(player.getName());
+        if (openMenu != null) {
+            openMenu.onClose(player);
+            Menu.currentlyOpenedMenus.remove(player.getName());
+        }
+
+        player.setMetadata("scanglitch", new FixedMetadataValue(SoupPvP.getInstance(), true));
+    }
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        if (player.hasMetadata("scanglitch")) {
+            player.removeMetadata("scanglitch", SoupPvP.getInstance());
+            for (ItemStack it : player.getInventory().getContents()) {
+                if (it != null) {
+                    ItemMeta meta = it.getItemMeta();
+                    if (meta != null && meta.hasDisplayName()) {
+                        if (meta.getDisplayName().contains("§b§c§d§e")) {
+                            player.getInventory().remove(it);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/me/elb1to/souppvp/utils/menu/Menu.java
@@ -0,0 +1,140 @@
+package me.elb1to.souppvp.utils.menu;
+
+import lombok.Getter;
+import lombok.Setter;
+import me.elb1to.souppvp.SoupPvP;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.scheduler.BukkitTask;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter @Setter
+public abstract class Menu {
+
+    public static Map<String, Menu> currentlyOpenedMenus = new HashMap<>();
+    @Getter private Map<Integer, Button> buttons = new HashMap<>();
+
+    private boolean autoUpdate = false;
+    private boolean updateAfterClick = true;
+    private boolean closedByMenu = false;
+    private boolean placeholder = false;
+
+    private Button placeholderButton = Button.placeholder(Material.STAINED_GLASS_PANE, (byte) 7, " ");
+    private BukkitTask task;
+
+    private ItemStack createItemStack(Player player, Button button) {
+        ItemStack item = button.getButtonItem(player);
+        if (item.getType() != Material.SKULL) {
+            ItemMeta meta = item.getItemMeta();
+            if (meta != null && meta.hasDisplayName()) {
+                meta.setDisplayName(meta.getDisplayName() + "§b§c§d§e");
+            }
+            item.setItemMeta(meta);
+        }
+
+        return item;
+    }
+
+    public void openMenu(final Player player) {
+        this.buttons = this.getButtons(player);
+
+        Menu previousMenu = Menu.currentlyOpenedMenus.get(player.getName());
+        Inventory inventory = null;
+        int size = this.getSize() == -1 ? this.size(this.buttons) : this.getSize();
+        boolean update = false;
+        String title = this.getTitle(player);
+
+        if (title.length() > 32) {
+            title = title.substring(0, 32);
+        }
+
+        if (player.getOpenInventory() != null) {
+            if (previousMenu == null) {
+                player.closeInventory();
+            } else {
+                int previousSize = player.getOpenInventory().getTopInventory().getSize();
+
+                if (previousSize == size && player.getOpenInventory().getTopInventory().getTitle().equals(title)) {
+                    inventory = player.getOpenInventory().getTopInventory();
+                    update = true;
+                } else {
+                    previousMenu.setClosedByMenu(true);
+                    player.closeInventory();
+                }
+            }
+        }
+
+        if (inventory == null) {
+            inventory = Bukkit.createInventory(player, size, title);
+        }
+
+        inventory.setContents(new ItemStack[inventory.getSize()]);
+
+        currentlyOpenedMenus.put(player.getName(), this);
+
+        for (Map.Entry<Integer, Button> buttonEntry : this.buttons.entrySet()) {
+            inventory.setItem(buttonEntry.getKey(), createItemStack(player, buttonEntry.getValue()));
+        }
+
+        if (this.isPlaceholder()) {
+            for (int index = 0; index < size; index++) {
+                if (this.buttons.get(index) == null) {
+                    this.buttons.put(index, this.placeholderButton);
+                    inventory.setItem(index, this.placeholderButton.getButtonItem(player));
+                }
+            }
+        }
+
+        if (update) {
+            player.updateInventory();
+        } else {
+            player.openInventory(inventory);
+        }
+
+        this.onOpen(player);
+        this.setClosedByMenu(false);
+        if (autoUpdate && task == null) {
+            task = SoupPvP.getInstance().getServer().getScheduler().runTaskTimer(SoupPvP.getInstance(), () -> this.openMenu(player), 0, 20L);
+        }
+    }
+
+    public int size(Map<Integer, Button> buttons) {
+        int highest = 0;
+
+        for (int buttonValue : buttons.keySet()) {
+            if (buttonValue > highest) {
+                highest = buttonValue;
+            }
+        }
+
+        return (int) (Math.ceil((highest + 1) / 9D) * 9D);
+    }
+
+    public int getSlot(int x, int y) {
+        return ((9 * y) + x);
+    }
+
+    public int getSize() {
+        return -1;
+    }
+
+    public abstract String getTitle(Player player);
+
+    public abstract Map<Integer, Button> getButtons(Player player);
+
+    public void onOpen(Player player) {
+
+    }
+
+    public void onClose(Player player) {
+        if (task != null) {
+            task.cancel();
+        }
+    }
+}
