fe9c80c1e75ec73fda6015c5c50554cb8fff2fe5
Elb1to <elbitomc@gmail.com>
2021-05-13 18:16:03 -0500

    Created Kit Selection Menu

---
+++ src/main/java/me/elb1to/souppvp/user/ui/kit/KitSelectionMenu.java
@@ -0,0 +1,37 @@
+package me.elb1to.souppvp.user.ui.kit;
+
+import me.elb1to.souppvp.SoupPvP;
+import me.elb1to.souppvp.kit.Kit;
+import me.elb1to.souppvp.user.User;
+import me.elb1to.souppvp.user.ui.kit.selection.SelectKitButton;
+import me.elb1to.souppvp.utils.menu.Button;
+import me.elb1to.souppvp.utils.menu.Menu;
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Elb1to
+ * Project: SoupPvP
+ * Date: 5/13/2021 @ 12:38 PM
+ */
+public class KitSelectionMenu extends Menu {
+
+    @Override
+    public String getTitle(Player player) {
+        return "Kit Selection";
+    }
+
+    @Override
+    public Map<Integer, Button> getButtons(Player player) {
+        Map<Integer, Button> buttons = new HashMap<>();
+        User user = SoupPvP.getInstance().getUserManager().getByUuid(player.getUniqueId());
+
+        for (Kit kit : SoupPvP.getInstance().getKitManager().getKits()) {
+            buttons.put(buttons.size(), new SelectKitButton(kit, user));
+        }
+        
+        return buttons;
+    }
+}
