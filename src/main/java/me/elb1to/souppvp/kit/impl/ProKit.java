a3f61ae43ed94e90df865d5ca9ec71115e3ec537
Elb1to <elbitomc@gmail.com>
2021-05-13 12:02:45 -0500

    Create ProKit.java

---
+++ src/main/java/me/elb1to/souppvp/kit/impl/ProKit.java
@@ -0,0 +1,44 @@
+package me.elb1to.souppvp.kit.impl;
+
+import me.elb1to.souppvp.kit.Ability;
+import me.elb1to.souppvp.kit.Kit;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+
+/**
+ * Created by Elb1to
+ * Project: SoupPvP
+ * Date: 5/6/2021 @ 9:42 PM
+ */
+public class ProKit extends Kit {
+
+	public ProKit() {
+		super("Pro", null, 250);
+	}
+
+	@Override
+	public ItemStack[] getArmor() {
+		return new ItemStack[]{
+				new ItemStack(Material.IRON_HELMET),
+				new ItemStack(Material.IRON_CHESTPLATE),
+				new ItemStack(Material.IRON_LEGGINGS),
+				new ItemStack(Material.IRON_BOOTS),
+		};
+	}
+
+	@Override
+	public ItemStack getSword() {
+		return new ItemStack(Material.DIAMOND_SWORD);
+	}
+
+	@Override
+	public Ability getAbilityItem() {
+		return null;
+	}
+
+	@Override
+	public PotionEffect[] getPotionEffects() {
+		return new PotionEffect[0];
+	}
+}
