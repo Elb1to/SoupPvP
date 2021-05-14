de122399a7d3cdacc37a9936d5d9d74cf1bad2ae
Elb1to <elbitomc@gmail.com>
2021-05-14 10:47:27 -0500

    Kits will be loaded from the constructor now

--- src/main/java/me/elb1to/souppvp/kit/KitManager.java
+++ src/main/java/me/elb1to/souppvp/kit/KitManager.java
@@ -5,6 +5,7 @@ import me.elb1to.souppvp.kit.impl.DefaultKit;
 import me.elb1to.souppvp.kit.impl.ProKit;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Elb1to
@@ -14,20 +15,20 @@ import java.util.ArrayList;
 @Getter
 public class KitManager {
 
-	private final ArrayList<Kit> kits = new ArrayList<>();
+    private final List<Kit> kits = new ArrayList<>();
 
-	public Kit getKitByName(String name) {
-		for (Kit kit : kits) {
-			if (name.equals(kit.getName())) {
-				return kit;
-			}
-		}
+    public KitManager() {
+        kits.add(new DefaultKit());
+        kits.add(new ProKit());
+    }
 
-		return null;
-	}
+    public Kit getKitByName(String name) {
+        for (Kit kit : kits) {
+            if (name.equals(kit.getName())) {
+                return kit;
+            }
+        }
 
-	public void loadKits() {
-		kits.add(new DefaultKit());
-		kits.add(new ProKit());
-	}
+        return null;
+    }
 }
