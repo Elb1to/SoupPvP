8ea7f395d299546d754c93248dd3cb2a7c1ae37a
Elb1to <elbitomc@gmail.com>
2021-07-18 14:10:53 -0500

    PvP-Prevention while in spawn region & hotbar items interaction

--- src/main/java/me/elb1to/souppvp/listeners/GameListener.java
+++ src/main/java/me/elb1to/souppvp/listeners/GameListener.java
@@ -2,19 +2,20 @@ package me.elb1to.souppvp.listeners;
 
 import me.elb1to.souppvp.SoupPvP;
 import me.elb1to.souppvp.user.User;
+import me.elb1to.souppvp.user.ui.kit.KitSelectionMenu;
 import me.elb1to.souppvp.utils.CC;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-import static me.elb1to.souppvp.utils.PlayerUtil.resetHotbar;
-import static me.elb1to.souppvp.utils.PlayerUtil.resetPlayer;
+import static me.elb1to.souppvp.utils.PlayerUtil.*;
 
 /**
  * Created by Elb1to
@@ -31,6 +32,27 @@ public class GameListener implements Listener {
 
         resetPlayer(player);
         resetHotbar(player);
+    }
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+
+        if (event.getItem() == null) {
+            return;
+        }
+
+        if (event.getItem().equals(KIT_SELECTOR)) {
+            new KitSelectionMenu().openMenu(player);
+        } else if (event.getItem().equals(EVENT_HOSTING)) {
+            player.sendMessage("Open Events Menu");
+        } else if (event.getItem().equals(PLAYER_PERKS)) {
+            player.sendMessage("Open Perks Menu");
+        } else if (event.getItem().equals(PREVIOUS_KIT)) {
+            player.sendMessage("Choose previous kit");
+        } else if (event.getItem().getType().equals(Material.SKULL_ITEM)) {
+            player.sendMessage("Open Stats Menu");
+        }
     }
 
     @EventHandler
@@ -56,6 +78,23 @@ public class GameListener implements Listener {
             User dUser = this.plugin.getUserManager().getByUuid(player.getUniqueId());
             dUser.setDeaths(dUser.getDeaths() + 1);
         }
+
+        resetPlayer(player);
+        resetHotbar(player);
+    }
+
+    @EventHandler
+    public void onPlayerDamage(EntityDamageByEntityEvent event) {
+        if (!(event.getDamager() instanceof Player)) {
+            return;
+        }
+
+        Player victim = (Player) event.getEntity();
+        Player attacker = (Player) event.getDamager();
+
+        if (inCuboid(victim) && inCuboid(attacker) || !inCuboid(victim) && inCuboid(attacker) || inCuboid(victim) && !inCuboid(attacker)) {
+            event.setCancelled(true);
+        }
     }
 
     @EventHandler
@@ -74,5 +113,9 @@ public class GameListener implements Listener {
         if (event.getItemDrop().getItemStack().getType() == Material.BOWL) {
             event.getItemDrop().remove();
         }
+    }
+
+    private boolean inCuboid(Player player) {
+        return this.plugin.getSpawnController().getCuboid().isIn(player);
     }
 }
