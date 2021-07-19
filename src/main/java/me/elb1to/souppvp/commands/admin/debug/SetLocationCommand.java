a2799cf1a06059d54beac4ee78301464bdb264b6
Elb1to <elbitomc@gmail.com>
2021-07-19 11:15:51 -0500

    Codacy xd

--- src/main/java/me/elb1to/souppvp/commands/admin/debug/SetLocationCommand.java
+++ src/main/java/me/elb1to/souppvp/commands/admin/debug/SetLocationCommand.java
@@ -1,13 +1,12 @@
 package me.elb1to.souppvp.commands.admin.debug;
 
 import me.elb1to.souppvp.SoupPvP;
+import me.elb1to.souppvp.utils.ColorHelper;
 import me.elb1to.souppvp.utils.command.BaseCommand;
 import me.elb1to.souppvp.utils.command.Command;
 import me.elb1to.souppvp.utils.command.CommandArgs;
 import me.elb1to.souppvp.utils.cuboid.CustomLocation;
 import org.bukkit.entity.Player;
-
-import static me.elb1to.souppvp.utils.ColorHelper.translate;
 
 /**
  * Created by Elb1to
@@ -18,16 +17,13 @@ public class SetLocationCommand extends BaseCommand {
 
     private final SoupPvP plugin = SoupPvP.getInstance();
 
-    @Override @Command(name = "setlocation", permission = "soup-pvp.admin")
+    @Override @Command(name = "setlocation", permission = "soup-pvp.admin", usage = "&cUsage: /setlocation <spawn|min|max>")
     public void onCommand(CommandArgs command) {
         Player player = command.getPlayer();
         String[] args = command.getArgs();
 
         if (args.length == 0) {
-            player.sendMessage(translate("&b&lSet Spawn"));
-            player.sendMessage(translate("&8 * &f/setlocation spawn"));
-            player.sendMessage(translate("&8 * &f/setlocation min"));
-            player.sendMessage(translate("&8 * &f/setlocation max"));
+            player.sendMessage(ColorHelper.translate(command.getCommand().getUsage()));
             return;
         }
 
@@ -35,17 +31,20 @@ public class SetLocationCommand extends BaseCommand {
             case "spawn":
                 this.plugin.getSpawnController().setSpawnLocation(CustomLocation.fromBukkitLocation(player.getLocation()));
                 saveLocation(player, "SERVER.SPAWN.LOCATION");
-                player.sendMessage(translate("&aSuccessfully saved spawn location."));
+                player.sendMessage(ColorHelper.translate("&aSuccessfully saved spawn location."));
                 break;
             case "max":
                 this.plugin.getSpawnController().setSafezoneMax(CustomLocation.fromBukkitLocation(player.getLocation()));
                 saveLocation(player, "SERVER.SPAWN.SAFEZONE-MAX");
-                player.sendMessage(translate("&aSuccessfully saved safezone max location."));
+                player.sendMessage(ColorHelper.translate("&aSuccessfully saved safezone max location."));
                 break;
             case "min":
                 this.plugin.getSpawnController().setSafezoneMin(CustomLocation.fromBukkitLocation(player.getLocation()));
                 saveLocation(player, "SERVER.SPAWN.SAFEZONE-MIN");
-                player.sendMessage(translate("&aSuccessfully saved safezone min location."));
+                player.sendMessage(ColorHelper.translate("&aSuccessfully saved safezone min location."));
+                break;
+            default:
+                player.sendMessage(ColorHelper.translate(command.getCommand().getUsage()));
                 break;
         }
     }
