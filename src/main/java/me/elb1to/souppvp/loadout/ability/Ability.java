aa0d8595b576419c97fae9932a8b5530c94f7c73
Elb1to <elbitomc@gmail.com>
2021-07-23 17:08:50 -0500

    Create Ability.java

---
+++ src/main/java/me/elb1to/souppvp/loadout/ability/Ability.java
@@ -0,0 +1,26 @@
+package me.elb1to.souppvp.loadout.ability;
+
+import lombok.Getter;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+/**
+ * Created by Elb1to
+ * Project: SoupPvP
+ * Date: 5/6/2021 @ 9:26 PM
+ */
+@Getter
+public abstract class Ability {
+
+    private final String name;
+    private final ItemStack item;
+
+    public Ability(String name, ItemStack item) {
+        this.name = name;
+        this.item = item;
+    }
+
+    public abstract long getCooldown();
+
+    public abstract AbilityCallable getCallable();
+}
