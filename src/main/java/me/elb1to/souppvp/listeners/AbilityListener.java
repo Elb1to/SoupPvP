c56c2fb2653be8c243d5b7e0451ff10c2be45caa
Elb1to <elbitomc@gmail.com>
2021-07-23 19:41:19 -0500

    Create AbilityListener.java

---
+++ src/main/java/me/elb1to/souppvp/listeners/AbilityListener.java
@@ -0,0 +1,31 @@
+package me.elb1to.souppvp.listeners;
+
+import me.elb1to.souppvp.SoupPvP;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+/**
+ * Created by Elb1to
+ * Project: SoupPvP
+ * Date: 7/23/2021 @ 4:38 PM
+ */
+public class AbilityListener implements Listener {
+
+    private final SoupPvP plugin = SoupPvP.getInstance();
+
+    @EventHandler
+    public void onRightClick(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        if (event.getItem() == null) {
+            return;
+        }
+
+        if (event.getAction().name().startsWith("RIGHT_")) {
+            if (event.getItem().isSimilar(this.plugin.getAbilityManager().getAbilityByName("Shuriken").getItem())) {
+                this.plugin.getAbilityManager().getAbilityByName("Shuriken").getCallable().execute(player);
+            }
+        }
+    }
+}
