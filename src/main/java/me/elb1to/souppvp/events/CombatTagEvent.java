8f000f5821c94b721bb18a42424edeb67e505a29
Elb1to <elbitomc@gmail.com>
2021-09-15 09:51:06 -0500

    Update CombatTagEvent.java

--- src/main/java/me/elb1to/souppvp/events/CombatTagEvent.java
+++ src/main/java/me/elb1to/souppvp/events/CombatTagEvent.java
@@ -1,5 +1,6 @@
 package me.elb1to.souppvp.events;
 
+import lombok.Getter;
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.player.PlayerEvent;
@@ -8,9 +9,10 @@ import org.bukkit.event.player.PlayerEvent;
  * Created by Infames
  * Date: 23/07/2021 @ 20:07
  */
+@Getter
 public class CombatTagEvent extends PlayerEvent {
 
-    private static final HandlerList handlers = new HandlerList();
+    private static final HandlerList HANDLERS = new HandlerList();
     private final Player attacker;
 
     public CombatTagEvent(Player player, Player attacker) {
@@ -18,12 +20,12 @@ public class CombatTagEvent extends PlayerEvent {
         this.attacker = attacker;
     }
 
-    public Player getAttacker() {
-        return attacker;
+    public static HandlerList getHandlerList() {
+        return HANDLERS;
     }
 
     public HandlerList getHandlers() {
-        return handlers;
+        return HANDLERS;
     }
 }
 
